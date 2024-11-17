0f66612180c1ce4997d956c41200f0175b515894
gleyson003 <lop23031@byui.edu>
2024-11-17 02:33:36 -0300

    Project week 2: chamber of commerce.

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,3 +1,78 @@
+//Toggle Menu
+document.getElementById('menuToggle').addEventListener('click', function() {
+    document.getElementById('menu').classList.toggle('active');
+});
+
+
+//Companies Cards
+// Função para buscar as informações das organizações e criar os cards
+async function fetchCompanies() {
+    try {
+        
+        const response = await fetch('/chamber/data/members.json'); 
+        const data = await response.json(); 
+        
+        const gridContainer = document.querySelector('.grid');
+        
+       
+        gridContainer.innerHTML = '';
+
+        
+        data.forEach(company => {
+            
+            const card = document.createElement('section');
+            card.classList.add('company-card'); 
+
+            const icon = document.createElement('img');
+            icon.src = `images/companies_logos/${company.icon}`; 
+            icon.alt = company.name;
+            card.appendChild(icon);
+
+            const address = document.createElement('p');
+            address.textContent = `Adress: ${company.adresse}`;
+            card.appendChild(address);
+
+            const phone = document.createElement('p');
+            phone.textContent = `Phone: ${company.phone}`;
+            card.appendChild(phone);
+
+            const website = document.createElement('a');
+            website.href = company.website;
+            website.target = '_blank';
+            website.textContent = company.website;
+            card.appendChild(website);
+
+            gridContainer.appendChild(card);
+        });
+    } catch (error) {
+        console.error("Erro ao carregar os dados das empresas:", error);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', fetchCompanies);
+
+
+
+
+//Filter function
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+gridbutton.addEventListener("click", () => {
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList);
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+}
+
+
+
 // Footer current year and moddification
 document.addEventListener('DOMContentLoaded', function() {
     let currentYear = new Date().getFullYear();
